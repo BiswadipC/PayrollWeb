4fc3797e0d4d1f49a6a0e785676dcdec7573836c
Biswadip <biswadip100@gmail.com>
2026-08-18 11:10:18 +0530

    import { environment } from '../../environments/environment'

--- src/app/Services/designation-services.ts
+++ src/app/Services/designation-services.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { IDesignation } from '../ClassesAndInterfaces/IDesignation';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
